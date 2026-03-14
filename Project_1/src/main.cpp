0401b8d03234a1321480bfdd955a8a54c379e3b7
mlody-jano <jasiu.hulboj@gmail.com>
2026-03-14 13:35:20 +0100

    redefinition of project structure, mainly regarding diversifying the responsibilities to more classes

--- Project_1/src/main.cpp
+++ Project_1/src/main.cpp
@@ -1,16 +1,22 @@
-#include "declarations.hpp"
+#include "Load.hpp"
+#include "Sort.hpp"
+
+struct Rank {
+    string name;
+    int ranking;
+};
 
 int main(int argc, char *argv[]) {
 
     validDataGuard(argc, argv);
 
-    Solution solution;
+    Load loader;
 
-    solution.setTable(atoi(argv[1]), atoi(argv[2]), atoi(argv[3]), atoi(argv[4]), atoi(argv[5]));
+    loader.setTable(atoi(argv[1]), atoi(argv[2]), atoi(argv[3]), atoi(argv[4]), atoi(argv[5]));
 
     while(true) {
-        solution.showMenu();
-        solution.catchOptions();
+        loader.showMenu();
+        loader.catchOptions();
     }
 
 }
